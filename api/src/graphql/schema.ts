3042a1f1631f535d74843c6581953e2c9a6ace07
jrquadros <quadrosjunioradits@gmail.com>
2019-04-30 00:05:44 -0300

    first commit

---
+++ api/src/graphql/schema.ts
@@ -0,0 +1,44 @@
+import { Query } from './query';
+import { Mutation } from './mutation';
+
+import { commentTypes } from './resources/comment/comment.schema';
+import { postTypes } from './resources/post/post.schema';
+import { userTypes } from './resources/user/user.schema';
+import { tokenTypes } from './resources/token/token.schema';
+
+import { commentResolvers } from './resources/comment/comment.resolvers';
+import { postResolvers } from './resources/post/post.resolvers';
+import { userResolvers } from './resources/user/user.resolvers';
+import { tokenResolvers } from './resources/token/token.resolvers';
+
+import { makeExecutableSchema } from 'graphql-tools'    
+import { merge } from 'lodash';
+
+
+
+const resolvers = merge(
+    commentResolvers,
+    postResolvers,
+    tokenResolvers,
+    userResolvers
+);
+
+const SchemaDefinition = `
+    type Schema {
+        query: Query
+        mutation: Mutation
+    }
+`;
+
+export default makeExecutableSchema({
+    typeDefs: [
+        SchemaDefinition,
+        Query,
+        Mutation,
+        commentTypes,
+        postTypes,
+        tokenTypes,
+        userTypes
+    ],
+    resolvers
+});
