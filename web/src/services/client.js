d624c39161a62f5ca24205a3ef1ceffc7c4cda5b
jrquadros <quadrosjunioradits@gmail.com>
2019-05-08 16:25:39 -0300

    apollo client added

---
+++ web/src/services/client.js
@@ -0,0 +1,30 @@
+import { ApolloClient, ApolloLink, InMemoryCache, HttpLink } from 'apollo-boost';
+
+const httpLink = new HttpLink({uri: 'http://localhost:8080/graphql'});
+
+const authLink = new ApolloLink((operation, forward) => {
+    //Retrieve the authorization token from local storage
+
+    const token = localStorage.getItem('access-token');
+        const uid = localStorage.getItem('uid');
+        const client = localStorage.getItem('client');
+
+        //Use the setContext method to set HTTP headers
+
+        operation.setContext({
+            headers: {
+                'access-token': token ? `${token}` : '',
+                'uid': uid ? `${uid}` : '',
+                'client': client ? `${client}` : ''
+            }
+        });
+
+        return forward(operation);
+});
+
+const client = new ApolloClient({
+    link: authLink.concat(httpLink),
+    cache: new InMemoryCache()
+});
+
+export default client;
