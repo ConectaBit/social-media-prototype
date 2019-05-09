6e8efd4c5ef00690626a0d1854a75142af9fd0d8
jrquadros <quadrosjunioradits@gmail.com>
2019-05-09 00:06:42 -0300

    apollo provider added

--- web/src/index.js
+++ web/src/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-
-ReactDOM.render(<App />, document.getElementById('root'));
+import client from './services/client'
+import { ApolloProvider } from 'react-apollo';
+ReactDOM.render(
+    <ApolloProvider client={client}>
+        <App />
+    </ApolloProvider>,
+    document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
