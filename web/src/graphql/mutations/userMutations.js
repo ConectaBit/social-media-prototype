f8f883b426224357adf8e6b94786a94c48faa34a
jrquadros <quadrosjunioradits@gmail.com>
2019-05-09 00:39:14 -0300

    user mutations

---
+++ web/src/graphql/mutations/userMutations.js
@@ -0,0 +1,21 @@
+import gql from 'graphql-tag';
+
+export const CREATE_TOKEN = gql`
+  mutation createToken($email: String!, $password: String!) {
+      token
+    }
+`;
+
+export const CREATE_USER = gql`
+    mutation createUser(input: {
+        $name: String!
+        $email: String!
+        $password: String! 
+    }){
+      id
+      name
+      email
+      photo
+      createdAt
+    }
+`;
