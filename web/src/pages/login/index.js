bf41a87d7b985412eaca6693a29bcaad7c1bd403
jrquadros <quadrosjunioradits@gmail.com>
2019-05-11 03:36:41 -0300

    createToken added

--- web/src/pages/login/index.js
+++ web/src/pages/login/index.js
@@ -1,24 +1,67 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {Box, Input, Button, Flex} from '../../components/basics/styles'
 import Header from '../../components/header'
 import { NavLink } from 'react-router-dom';
+import gql from 'graphql-tag';
+import { Mutation } from 'react-apollo';
 
 function Login() {
+
+  const [email, setEmail] = useState('');
+  const [pass, setPass] = useState('');
+  const [token, setToken] = useState(null);
+
+  const CREATE_TOKEN = gql`
+    mutation createToken($email: String!, $password: String!){
+      createToken(email: $email, password: $password){
+        token
+      }
+    }
+  `;
+
+
+
   return(
-    <>
+    <Mutation mutation={CREATE_TOKEN}>
+    {(createToken, {data, loading, error}) => {
+      if(data){
+        console.log(data)
+        setToken(data.createToken.token)
+        return <>Deu certo | Token: {token}</>
+      }
+
+      if(loading){
+        return <>Loading...</>
+      }
+
+      if(error){
+        return <>Errrou</>
+        console.log(error)
+      }
+   return <>
       <Header />
       <Box direction='column'>
         <h1>Login</h1>
         <Flex direction='column'>
-          <Input placeholder='Email' type='email' />
-          <Input placeholder='Senha' type='password' />
-          <Flex direction='column' align='center'>
-            <Button value='submit' type='submit'>Entrar</Button>
+          <form onSubmit={e => {
+            createToken({
+              variables: {
+                email: email,
+                password: pass
+              }
+            })
+          }}>
+          <Input placeholder='Email' type='email'   onChange={e => setEmail(e.target.value)}/>
+          <Input placeholder='Senha' type='password'onChange={e => setPass(e.target.value)} />
+          <Button value='submit' type='submit'>Entrar</Button>
             <NavLink to='/' replace>Cadastre-se</NavLink>
-          </Flex>
+          </form>
         </Flex>
       </Box>
     </>
+    }}
+    </Mutation>
+
   );
 }
 
