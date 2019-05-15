0b6ad0fb633b3b64fc78eda37f874e88b045e9ce
jrquadros <quadrosjunioradits@gmail.com>
2019-05-15 19:08:01 -0300

    Local storage token verify on routes

--- web/src/routes.js
+++ web/src/routes.js
@@ -3,15 +3,19 @@ import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 import Register from "./pages/register";
 import Login from "./pages/login";
-import Feed from './pages/feed';
+import Feed from "./pages/feed";
+
+function isAuth() {
+  return localStorage.getItem("access-token") ? true : false;
+}
 
 function Routes() {
   return (
     <BrowserRouter>
       <Switch>
-        <Route path="/login" component={Login} />
+        <Route path="/register" component={isAuth() ? Feed : Register} />
         <Route path="/feed" component={Feed} />
-        <Route exat={true} path="/" component={Register} />
+        <Route exat={true} path="/" component={isAuth() ? Feed : Login} />
       </Switch>
     </BrowserRouter>
   );
