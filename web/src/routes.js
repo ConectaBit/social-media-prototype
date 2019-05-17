fce0a9efb10b8a21e1f774deb49e4fb5dc88a40d
jrquadros <quadrosjunioradits@gmail.com>
2019-05-17 14:45:19 -0300

    fix auth routes

--- web/src/routes.js
+++ web/src/routes.js
@@ -5,27 +5,33 @@ import Register from "./pages/register";
 import Login from "./pages/login";
 import Logout from './pages/logout'
 import Feed from "./pages/feed";
-
-const isAuth = localStorage.getItem("access-token") ? true : false;
+import Public from './pages/public'
 
 function Routes() {
+
+  function isAuth(){
+    return localStorage.getItem("access-token") ? true : false;
+  }
   return (
     <BrowserRouter>
       <Switch>
         <Route path='/register' render={() => (
-          isAuth
-          ? (<Redirect to='/feed/'/>)
+          isAuth()
+          ? (<Redirect to='/home/'/>)
           : (<Register />)
         )}/>
-        
-        <Route path="/feed/" component={isAuth ? Feed : Login} />
-        
+      
+        <Route path='/home' render={() => (
+          isAuth()
+          ? (<Feed />)
+          : (<Login />)
+        )} />
         <Route path='/logout' component={Logout}/>
         
         <Route exat={true} path="/" render={() => (
-          isAuth
-          ? (<Redirect to='feed'/>)
-          : (<Login />)
+          isAuth()
+          ? (<Redirect to='home'/>)
+          : (<Public />)
         )} />
       </Switch>
     </BrowserRouter>
