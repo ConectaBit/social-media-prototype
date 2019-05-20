8ff6b39633b8e157aa3575fab58565ccbf2efdcf
jrquadros <quadrosjunioradits@gmail.com>
2019-05-20 02:55:13 -0300

    pagination actions

--- web/src/components/postList/index.js
+++ web/src/components/postList/index.js
@@ -1,15 +1,30 @@
-import React, { useState } from "react";
-import { Box, Flex } from "../basics/styles";
+import React, { useState, useReducer } from "react";
+import { Box, Flex, Button } from "../basics/styles";
 
 import gql from "graphql-tag";
 import { Query } from "react-apollo";
 
+const initialState = 0;
+const reducer = (state, action) => {
+  switch (action) {
+    case "increment":
+      return state + 1;
+    case "decrement":
+      return state - 1;
+    case "reset":
+      return 0;
+    default:
+      throw new Error("Unexpected action");
+  }
+};
+
 function PostList() {
   const [posts, setPosts] = useState([]);
+  const [count, dispatch] = useReducer(reducer, initialState);
 
   const POST_LIST = gql`
-    query {
-      posts(first: 30, offset: 0) {
+    query GetPosts($first: Int!, $offset: Int!){
+      posts(first: $first, offset: $offset) {
         id
         title
         content
@@ -24,7 +39,7 @@ function PostList() {
 
   return (
     <>
-      <Query query={POST_LIST}>
+      <Query query={POST_LIST} variables={{first: 10, offset: count * 10}}>
         {({ loading, error, data }) => {
           if (loading) {
             return <>Loading...</>;
@@ -38,18 +53,22 @@ function PostList() {
             setPosts(data.posts);
           }
 
-          return (
-            posts.map((posts) => (<Box key={posts.id}>
-                <Flex direction='column'>
-                    <h1>{posts.title}</h1>
-                    <h3>{posts.content}</h3>
-                    <span>Autor: {posts.author.name}</span>
-                    <span>Updatad at: {posts.updatedAt}</span>
-                </Flex>
-            </Box>))
-          );
+          return posts.map(posts => (
+            <Box key={posts.id}>
+              <Flex direction="column">
+                <h1>{posts.title}</h1>
+                <h3>{posts.content}</h3>
+                <span>Autor: {posts.author.name}</span>
+                <span>Updatad at: {posts.updatedAt}</span>
+              </Flex>
+            </Box>
+          ));
         }}
       </Query>
+
+      <Button onClick={() => dispatch('decrement')}>Anterior</Button>
+      {}
+      <Button onClick={() => dispatch('increment')}>Próximo</Button>
     </>
   );
 }
