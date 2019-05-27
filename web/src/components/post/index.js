b116298f361cce85a92e2c6b1892e4bdab273597
jrquadros <quadrosjunioradits@gmail.com>
2019-05-27 16:39:11 -0300

    get a unique post

---
+++ web/src/components/post/index.js
@@ -0,0 +1,48 @@
+import React from "react";
+import { Box, Flex } from "../basics/styles";
+
+import gql from "graphql-tag";
+import { Query } from "react-apollo";
+
+function Post(props) {
+  const GET_POST = gql`
+    query getPost($id: ID!) {
+      post(id: $id) {
+        id
+        title
+        content
+      }
+    }
+  `;
+
+  const getPostId = () => {
+    return parseInt(props.postId.slice(3));
+  };
+
+  return (
+    <Query query={GET_POST} variables={{ id: getPostId() }}>
+      {({ loading, error, data }) => {
+        if (loading) {
+          return <>Carregando...</>;
+        }
+
+        if (error) {
+          console.log(error);
+        }
+
+        const postInfo = data.post;
+
+        return (
+          <Box>
+            <Flex direction="column">
+              <h1>{postInfo.title}</h1>
+              <p>{postInfo.content}</p>
+            </Flex>
+          </Box>
+        );
+      }}
+    </Query>
+  );
+}
+
+export default Post;
--- web/src/pages/postDetails/index.js
+++ web/src/pages/postDetails/index.js
@@ -1,10 +1,13 @@
 import React from "react";
+
 import Header from "../../components/header";
+import Post from '../../components/post';
 
 function PostDetails(props) {
   return (
     <>
       <Header />
+      <Post postId={props.postId}/>
     </>
   );
 }
