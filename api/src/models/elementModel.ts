d116f6a5a4653e530e752337cee16c617fbea0e1
jrquadros <quadrosjunioradits@gmail.com>
2019-06-24 23:12:12 -0300

    table model

---
+++ api/src/models/elementModel.ts
@@ -0,0 +1,51 @@
+import * as Sequelize from "sequelize";
+
+import { BaseModelInterface } from "../interfaces/BaseModelInterface";
+import sequelize = require("sequelize");
+import { ModelsInterface } from "../interfaces/ModelsInterface";
+
+export interface ElementAttributes {
+  id?: number;
+  description?: string;
+  name?: string;
+  tags?: string[];
+  createdAt?: string;
+  updatedAt?: string;
+}
+
+export interface ElementInstance
+  extends Sequelize.Instance<ElementAttributes> {}
+
+export interface ElementModel
+  extends BaseModelInterface,
+    Sequelize.Model<ElementInstance, ElementAttributes> {}
+
+export default (Sequelize: Sequelize.Sequelize, DataTypes: Sequelize.DataTypes): ElementModel => {
+    const Element: ElementModel = Sequelize.define('Element', {
+        id:{
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+
+        description: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+
+        tags: {
+            type: DataTypes.ARRAY,
+            allowNull: true
+        }
+    }, {
+        tableName: 'elements'
+    });
+
+    return Element;
+}
