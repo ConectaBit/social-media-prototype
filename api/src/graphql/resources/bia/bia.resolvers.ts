da227c027dceceba9209647bedabecbb54f2c3be
jrquadros <quadrosjunioradits@gmail.com>
2019-07-04 09:59:16 -0300

    feat(bia): query on mathematics route

--- api/src/graphql/resources/bia/bia.resolvers.ts
+++ api/src/graphql/resources/bia/bia.resolvers.ts
@@ -1,11 +1,23 @@
 import { GraphQLResolveInfo } from "graphql";
-const fetch = require('isomorphic-fetch')
+const fetch = require("isomorphic-fetch");
 
 export const biaResolvers = {
   Query: {
     biology: (parent, { start = 1 }, info: GraphQLResolveInfo) => {
-      return fetch(`https://bia-api.herokuapp.com/api/biology/1`).then(res =>
-        res.json()
+      return fetch(`https://bia-api.herokuapp.com/api/biology/${start}`).then(
+        res => res.json()
+      );
+    },
+
+    chemistry: (parent, { start = 1 }, info: GraphQLResolveInfo) => {
+      return fetch(`https://bia-api.herokuapp.com/api/chemistry/${start}`).then(
+        res => res.json()
+      );
+    },
+
+    mathematics: (parent, { start = 1 }, info: GraphQLResolveInfo) => {
+      return fetch(`https://bia-api.herokuapp.com/api/mathematics/${start}`).then(
+        res => res.json()
       );
     }
   }
